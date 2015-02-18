8d188da82be0faa0995ecd35cca6bac103d1f482
Justin Terry <justin.terry@loop.colum.edu>
2015-02-18 14:15:58 -0600

    Revert "Version 1.0"
    This reverts commit 3807b0ce9982cf736afe55f1e477e9d74a8a269e.

--- CollisionFunctions.h
+++
@@ -1,9 +0,0 @@
-#ifndef COLLISIONFUNCTIONS_H
-#define COLLISIONFUNCTIONS_H
-
-#include "src\Circle.h"
-
-bool CollisonCheck(Circle object1, Circle object2);
-void newVelocityCalc(Circle object1, Circle object2);
-
-#endif
